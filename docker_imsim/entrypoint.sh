d67c44be74790aff555fb3aca52a5fc40ef51eea
Francois <francois@orioncloud.fr>
2018-09-03 16:06:36 -0700

    Adds modifs to imsim image

--- docker_imsim/entrypoint.sh
+++ docker_imsim/entrypoint.sh
@@ -5,9 +5,7 @@ source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_sims
 setup sims_GalSimInterface
 setup imsim
-setup twinkles
-setup sims_GCRCatSimInterface
-export PATH=$PATH:/build/imSim/bin.src:/build/sims_GCRCatSimInterface/bin.src
+export PATH=$PATH:/build/imSim/bin.src
 
 # Hand off to the CMD
 exec "$@"
